93e97074285364f2cda376a62f2d048aee6a7e37
guptakuhu <f20201524@goa.bits-pilani.ac.in>
2024-06-09 23:38:25 +0530

    Add zod validation file

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { createTodo, updateTodo } = require("./type");
 const app = express;
 
 app.use(express.json());
@@ -6,13 +7,26 @@ app.use(express.json());
 //body{ title: string, description: string}
 
 app.post("/todo", function(req,res) {
-
+    const createPayload = req.body
+    const parsePayload = createTodo.safeParse(createPayload)
+    if(!parsePayload.success) {
+        res.status(411).json({
+            msg: "You sent the wrong input"
+        })
+        return;
+    }
 })
 
 app.get("/todos", function(req,res) {
-    
+
 })
 
 app.put("/completed", function(req,res) {
-
+    const updatePayload = req.body
+    const parsePayload = updateTodo.safeParse(updatePayload)
+    if(!parsePayload.success) {
+        res.status(411).json({
+            msg: "You sent the wrong input"
+        })
+    }
 })
